aa14d753233cd346ac6abedf6ec82744377c4a18
Chris Johnston <chris14b@gmail.com>
2019-01-20 11:58:02 +1100

    new algorithm for converting frequency bins into tile bins

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ import DiamondTile from "./diamond-tile.js";
 
 const NUM_FREQUENCY_BINS = 16;
 const NUM_TILES = NUM_FREQUENCY_BINS;
+const NUM_TILE_GROUPS = 6;
 const MAX_HUE = 360;
 const FRAME_HUE_RANGE = 90; // range of hues to be shown at any one time
 const HUE_INCREMENT = 0.1; // hue range will shift by this amount every frame
@@ -24,7 +25,7 @@ window.onload = function() {
     canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
     // const tiles = createDiamondTiles(canvas, canvasContext);
-    const tiles = createGroupDiamondTiles(canvas, canvasContext);
+    const tiles = createGroupDiamondTiles2(canvas, canvasContext);
     // const tiles = createRectangleTiles(canvas, canvasContext);
     const tileHandler = new TileHandler(tiles);
 
@@ -39,13 +40,13 @@ window.onload = function() {
         source.connect(analyser);
         analyser.connect(audioContext.destination);
         analyser.fftSize = NUM_FREQUENCY_BINS * 2;
-        const dataArray = new Uint8Array(NUM_FREQUENCY_BINS);
+        const frequencyBins = new Uint8Array(NUM_FREQUENCY_BINS);
         let bin1Hue = Math.random() * MAX_HUE;
         let averageIntensities = [];
         let averageIntensitiesCount = 0;
         let maxIntensities = [];
 
-        for (let i = 0; i < NUM_TILES; i++) {
+        for (let i = 0; i < NUM_TILE_GROUPS; i++) {
             averageIntensities.push(0);
             maxIntensities.push(0);
         }
@@ -54,29 +55,28 @@ window.onload = function() {
 
         function renderFrame() {
             requestAnimationFrame(renderFrame);
-            analyser.getByteFrequencyData(dataArray);
+            analyser.getByteFrequencyData(frequencyBins);
             canvasContext.fillStyle = "black";
             canvasContext.fillRect(0, 0, canvas.width, canvas.height);
             let totalAlpha = 0;
             averageIntensitiesCount++;
+            let tileGroupBins = getTileBins(frequencyBins);
 
-            for (let i = 0; i < NUM_TILES; i++) {
-                averageIntensities[i] += (dataArray[i] - averageIntensities[i]) / averageIntensitiesCount;
-                maxIntensities[i] = Math.max(maxIntensities[i], dataArray[i]);
+            for (let i = 0; i < NUM_TILE_GROUPS; i++) {
+                averageIntensities[i] += (tileGroupBins[i] - averageIntensities[i]) / averageIntensitiesCount;
+                maxIntensities[i] = Math.max(maxIntensities[i], tileGroupBins[i]);
 
-                const hue = (i / NUM_TILES * FRAME_HUE_RANGE + bin1Hue) % MAX_HUE;
-                const alpha = scaleValue(dataArray[i], averageIntensities[i], maxIntensities[i], MIN_ALPHA, MAX_ALPHA);
+                const hue = (i / NUM_TILE_GROUPS * FRAME_HUE_RANGE + bin1Hue) % MAX_HUE;
+                const alpha = scaleValue(tileGroupBins[i], averageIntensities[i], maxIntensities[i], MIN_ALPHA, MAX_ALPHA);
 
                 if (!isNaN(alpha)) {
                     totalAlpha += alpha;
                 }
 
-                if (i % 4 === 0) {
-                    tileHandler.draw(i / 4, hue, alpha);
-                }
+                tileHandler.draw(i, hue, alpha);
             }
 
-            if (totalAlpha === MIN_ALPHA * NUM_TILES) {
+            if (totalAlpha === MIN_ALPHA * NUM_TILE_GROUPS) {
                 darkFrameCount++;
                 console.log(darkFrameCount);
             } else {
@@ -94,6 +94,44 @@ window.onload = function() {
         renderFrame();
     };
 };
+
+function getTileBins(frequencyBins) {
+    let tileGroupBins = [];
+
+    for (let i = 0; i < NUM_TILE_GROUPS; i++) {
+        tileGroupBins.push(0);
+        const minBin = i / NUM_TILE_GROUPS * NUM_FREQUENCY_BINS;
+        const maxBin = (i + 1) / NUM_TILE_GROUPS * NUM_FREQUENCY_BINS;
+
+        for (let j = 0; j < NUM_FREQUENCY_BINS; j++) {
+            let lowerThreshold;
+            let upperThreshold;
+
+            if (minBin < j) {
+                lowerThreshold = 0;
+            } else if (minBin > j + 1) {
+                lowerThreshold = 1;
+            } else {
+                lowerThreshold  = minBin - j;
+            }
+
+            if (maxBin < j) {
+                upperThreshold = 0;
+            } else if (maxBin > j + 1) {
+                upperThreshold = 1;
+            } else {
+                upperThreshold = maxBin - j;
+            }
+
+            const amount = upperThreshold - lowerThreshold;
+            tileGroupBins[i] += frequencyBins[j] * amount;
+        }
+
+        tileGroupBins[i] *= NUM_TILE_GROUPS / NUM_FREQUENCY_BINS;
+    }
+
+    return tileGroupBins;
+}
 
 function scaleValue(value, minIn, maxIn, minOut, maxOut) {
     return Math.max((value - minIn) / (maxIn - minIn) * (maxOut - minOut) + minOut, 0);
@@ -156,6 +194,41 @@ function createGroupDiamondTiles(canvas, canvasContext) {
             id = 2;
         } else {
             id = 3;
+        }
+
+        const tileX = canvas.width / 2 - tileWidth * 3 + tileWidth / 2 * i;
+
+        if (i % 2 === 0) {
+            tiles.push(new DiamondTile(id, canvasContext, tileX, canvas.height / 2 - tileHeight / 2, tileWidth, tileHeight));
+        } else {
+            tiles.push(new DiamondTile(id, canvasContext, tileX, canvas.height / 2 - tileHeight, tileWidth, tileHeight));
+            tiles.push(new DiamondTile(id, canvasContext, tileX, canvas.height / 2, tileWidth, tileHeight));
+        }
+    }
+
+    return tiles;
+}
+
+function createGroupDiamondTiles2(canvas, canvasContext) {
+    const tiles = [];
+    const tileHeight = canvas.height * USABLE_HEIGHT_RATIO / 2;
+    const tileWidth = tileHeight * DIAMOND_RATIO;
+
+    for (let i = 0; i < 11; i++) {
+        let id;
+
+        if (i < 1 || i >= 10) {
+            id = 5;
+        } else if (i < 2 || i >= 9) {
+            id = 4;
+        } else if (i < 3 || i >= 8) {
+            id = 3;
+        } else if (i < 4 || i >= 7) {
+            id = 2;
+        } else if (i < 5 || i >= 6) {
+            id = 1;
+        } else {
+            id = 0;
         }
 
         const tileX = canvas.width / 2 - tileWidth * 3 + tileWidth / 2 * i;
